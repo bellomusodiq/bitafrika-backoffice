eb633a0fbc7d26beb1fba83fb99a65e361786982
Mayowa Bello <bmayowa25@gmail.com>
2023-03-18 10:06:36 +0100

    implemented headers

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Button from "../Button";
 import styles from "./Header.module.css";
 
 const Header: React.FC = () => (
   <nav className={styles.headerContainer}>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, maxime
-    aperiam. Officiis tempore corrupti laborum veniam, aliquid voluptatem
-    beatae, sint aspernatur, architecto recusandae adipisci atque eos ad
-    deleniti earum aut?
+    <div className={styles.headerLeft}>
+      <span className={styles.text}>Welcome, Emmanuel</span>
+      <div className={styles.divider} style={{ margin: "0 16px" }} />
+      <span className={styles.text}>Monday 10th Feb, 2023</span>
+    </div>
+    <div className={styles.headerRight}>
+      <img src="/icons/bell.svg" />
+      <div className={styles.divider} style={{ margin: "0 28px" }} />
+      <img src="/icons/GH.svg" />
+      <span className={styles.country}>Ghana</span>
+      <Button color="white">Log Out</Button>
+    </div>
   </nav>
 );
 
