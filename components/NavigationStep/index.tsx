981a38462c93f2a8e150a2660eb6b808bab09a7c
Habeeb abdulrahman <32376973+habeebtheprogrammer@users.noreply.github.com>
2023-03-22 09:48:42 -0700

    WIP

---
+++ components/NavigationStep/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "../Button";
+import styles from "./NavigationStep.module.css";
+
+const NavigationStep: React.FC = () => {
+  return (
+    <div className={styles.navigationStep}>
+      <div className={styles.navItems}>
+        <span>Overview</span>
+        <img src="/icons/arrow-right2.svg" />
+        <span>Available balances</span>
+        <img src="/icons/arrow-right2.svg" />
+        <span>Bitcoin</span>
+      </div>
+      <Button color="white">
+        <>
+          <img src="/icons/arrow-left.svg" />
+          Back to overview
+        </>
+      </Button>
+    </div>
+  );
+};
+
+export default NavigationStep;
---
+++ pages/assets/[id].tsx
@@ -0,0 +1,16 @@
+import PageLayout from "@/components/PageLayout";
+import { NextPage } from "next";
+import React from "react";
+import styles from "./assets.module.css";
+
+const AssetsDetail: NextPage = () => {
+  return (
+    <PageLayout>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptas
+      itaque optio neque reiciendis alias doloribus quo molestiae eligendi, sunt
+      voluptatibus. Minus reiciendis eaque iusto dicta, expedita a ut rem!
+    </PageLayout>
+  );
+};
+
+export default AssetsDetail;
