12da8f0843fd846119f90cfb5e833c3c1bbf0766
bellomayowa <bmayowa25@gmail.com>
2024-02-21 10:07:23 +0100

    fixed signing

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import Button from "../Button";
 import styles from "./Header.module.css";
+import { useRouter } from "next/router";
 
-const Header: React.FC = () => (
-  <nav className={styles.headerContainer}>
-    <div className={styles.headerLeft}>
-      <span className={styles.text}>Welcome, Emmanuel</span>
-      <div className={styles.divider} style={{ margin: "0 16px" }} />
-      <span className={styles.text}>Monday 10th Feb, 2023</span>
-    </div>
-    <div className={styles.headerRight}>
-      <img src="/icons/bell.svg" />
-      <div className={styles.divider} style={{ margin: "0 28px" }} />
-      <img src="/icons/GH.svg" />
-      <span className={styles.country}>Ghana</span>
-      <Button color="white">Log Out</Button>
-    </div>
-  </nav>
-);
+const Header: React.FC = () => {
+  const router = useRouter();
+  return (
+    <nav className={styles.headerContainer}>
+      <div className={styles.headerLeft}>
+        <span className={styles.text}>Welcome, Emmanuel</span>
+        <div className={styles.divider} style={{ margin: "0 16px" }} />
+        <span className={styles.text}>Monday 10th Feb, 2023</span>
+      </div>
+      <div className={styles.headerRight}>
+        <img src="/icons/bell.svg" />
+        <div className={styles.divider} style={{ margin: "0 28px" }} />
+        <img src="/icons/GH.svg" />
+        <span className={styles.country}>Ghana</span>
+        <Button
+          onClick={() => {
+            localStorage.removeItem("auth");
+            router.replace("/signin", "/signin");
+          }}
+          color="white"
+        >
+          Log Out
+        </Button>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
