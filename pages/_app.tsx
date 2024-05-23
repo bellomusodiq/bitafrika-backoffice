d974f7f3c9cc6d37ef2f59254097cfc29048946d
Lanre039 <rajilat1000@gmail.com>
2024-05-23 16:49:43 +0100

    added react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      // to avoid refetching immediately on the client
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <ToastContainer
         position="top-center"
@@ -20,6 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
         pauseOnHover
         theme="colored"
       />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
